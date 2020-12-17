209d315add412756fbe95c9083e136e1ec1bcf22
sgowda3 <sgowda7697@gmail.com>
2020-12-17 21:47:39 +0530

    rendering stops in index.ejs

--- NodejsWebApp1/index.js
+++ NodejsWebApp1/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 var path = require('path');
 const app = express();
+const data = require('./stops.json');
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index"); // index refers to index.ejs
+    res.render("index", {jsonobj : JSON.stringify(data)}); // index refers to index.ejs
 });
 
 app.listen(3000, () => {
